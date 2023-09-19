f1c609b1ec057f5b21b647f2843bc94875a3d611
Lindsay201291 <lindsay_201291@hotmail.com>
2023-09-19 11:28:41 -0400

    Cambios en archivo environment.prod.ts y agrregado archivo capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.paypalionic',
+  appName: 'paypal-ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: 'AVh6buR4Bg2rPhLJJ8bDK-4lFpWjl6GNgc4z-W9VfO7smW-PKg2Iv_Q-IuSqoLi1zgxkK-6hDZ600N8m'
 };
